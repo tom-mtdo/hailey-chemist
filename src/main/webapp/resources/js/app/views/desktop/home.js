578fb8e4b6e3cb4c8b29fc33229eb3c8a9a0d807
Tom Do <tom@sisuwellness.com>
2016-08-05 08:11:14 +1000

    icon

--- src/main/webapp/resources/js/app/views/desktop/home.js
+++ src/main/webapp/resources/js/app/views/desktop/home.js
@@ -5,16 +5,19 @@ define([
     'utilities',
     'jquery',
     'bootstrap',
-    'text!../../../../templates/desktop/home.html'
+    'text!../../../../templates/desktop/home.html',
+    'text!../../../../templates/desktop/product-icon.html'
 ], function (utilities,
 		jquery,
 		bootstrap,
-		homeTemplate) {
+		homeTemplate,
+		productIcon) {
 
     var HomeView = Backbone.View.extend({
         render:function () {
             utilities.applyTemplate($(this.el),homeTemplate,{});
             $('.carousel').carousel();
+            utilities.applyTemplate($('#featuredProducts'), productIcon)
             return this;
         }
     });

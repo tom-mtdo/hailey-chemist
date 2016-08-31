6462d39a469435ba3c207ac881599756792b89ba
Tom Do <tom@sisuwellness.com>
2016-08-31 17:28:22 +1000

    return path of a category

--- src/main/java/com/mtdo/haileychemist/rest/CategoryService.java
+++ src/main/java/com/mtdo/haileychemist/rest/CategoryService.java
@@ -31,78 +31,66 @@ public class CategoryService extends BaseEntityService<Category>{
 	protected Predicate[] extractPredicates(MultivaluedMap<String, String> queryParameters,
 			CriteriaBuilder criteriaBuilder, Root<Category> root) {
 		Predicate[] predicates = new Predicate[]{};
-		
+
 		return predicates;
 	}
-	
-//	return path to a category (secified by Id)
-//	if id = null then return 
+
+	//	return path to a category (secified by Id)
+	//	if id = null then return 
 	@Path("/path/{categoryId}")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-//	public Map<Integer, String> getPath(@PathParam("categoryId") int categoryId){
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	//	public Map<Integer, String> getPath(@PathParam("categoryId") int categoryId){
 	public Map<Integer, String> getPath(@PathParam("categoryId") int categoryId){
 
-//		List<Predicate> lstPredicates = new ArrayList<Predicate>();
-		
+		//		List<Predicate> lstPredicates = new ArrayList<Predicate>();
+
+		//		get entity manager
 		EntityManager em = getEntityManager();
-		CriteriaBuilder cb = em.getCriteriaBuilder();		
-		CriteriaQuery<Tuple> cq = cb.createTupleQuery();		
+		//		get criteia builder
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		//		create query helper which allow select only few columns of a table - using tuple
+		CriteriaQuery<Tuple> cq = cb.createTupleQuery();
+		//		from clauses
 		Root<Category> node = cq.from(Category.class);
 		Root<Category> parent = cq.from(Category.class);
-//		Predicate predicate = cb.equal( node.get("id"), categoryId );
+		//		Predicate predicate = cb.equal( node.get("id"), categoryId );
+
+		//		where clauses
 		Predicate[] predicates = new Predicate[]{
-//				cb.like( node.<String>get("name"), "%" )
 				cb.between( node.<Integer>get("lft"), parent.<Integer>get("lft"), parent.<Integer>get("rgt")),
 				cb.equal( node.get("id"), categoryId )
-				};
+		};
 		cq.where( predicates );
+		//		say which columns to select
 		cq.multiselect(
 				parent.get("id"),
 				parent.get("name") );
+		//		order by
 		cq.orderBy( cb.asc(parent.get("lft")) );
-				
+
+		//		create real query with all settings above
 		TypedQuery<Tuple> tq = em.createQuery(cq);
+		//		perform query and get result in a list of tuples
 		List<Tuple> qResult = tq.getResultList();
 		Map<Integer, String> result = new HashMap<Integer, String>();
-		
+
+		//		convert tuples to a desired type
+		String path ="";
 		for (Tuple tuple: qResult){
-			int id = tuple.get(0, Integer.class);
-			String name = tuple.get(1, String.class);
-			result.put(id, name);
+			//			int id = tuple.get(0, Integer.class);
+			//			if first then no need comma
+			if ( path.trim().length() < 1){
+				path = tuple.get(1, String.class);
+			} else {
+				path = path + ">" + tuple.get(1, String.class);	
+			}
 		}
-		
+		result.put(categoryId, path);
+
 		return result;
-		
-		
-////		Map<Integer, String> result = new HashMap<Integer, String>();
-//		List<Predicate> lstPredicates = new ArrayList<Predicate>();
-//		
-////		result.put(categoryId, "geting category path");
-//
-//		EntityManager em = getEntityManager();
-//		final CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();		
-//		final CriteriaQuery<Category> criteriaQuery = criteriaBuilder.createQuery(Category.class);
-//		Root<Category> node = criteriaQuery.from(Category.class);
-////		Root<Category> parent = criteriaQuery.from(Category.class);
-//		
-//		
-//		
-//		Predicate predicate = criteriaBuilder.equal( node.get("id"), categoryId );
-//		lstPredicates.add(predicate);
-//		Predicate[] arrPredicates = ((List<Predicate>)lstPredicates).toArray(new Predicate[lstPredicates.size()]);
-//		
-////		Predicate[] predicates = new Predicate[]{};
-//		criteriaQuery.select( criteriaBuilder.construct( Category.class, node.get("id"), node.get("name")) );
-////		criteriaQuery.select(criteriaQuery.getSelection()).where(arrPredicates);
-//		criteriaQuery.where(arrPredicates);
-//		
-//		TypedQuery<Category> query = em.createQuery(criteriaQuery);
-//		return query.getResultList();
-////		
-////		return result;
 	}
-	
-	
+
+
 
 }

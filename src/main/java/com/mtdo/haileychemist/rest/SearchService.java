18a779a2ca773879288d617fea002df27ffa9fb3
Tom Do <tom@sisuwellness.com>
2016-08-31 22:46:44 +1000

    few comments

--- src/main/java/com/mtdo/haileychemist/rest/SearchService.java
+++ src/main/java/com/mtdo/haileychemist/rest/SearchService.java
@@ -18,14 +18,15 @@ public class SearchService {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String searchProducts(@Context UriInfo uriInfo) {
-//	public SearchResult searchProducts(@Context UriInfo uriInfo) {
+		//	public SearchResult searchProducts(@Context UriInfo uriInfo) {
 		String result = "";
-//		Get parameter
-//		http://localhost:8080/hailey-chemist/rest/search?categoryId=2&keyWord=abc
+		//		Get parameter
+		//		http://localhost:8080/hailey-chemist/rest/search?categoryId=2&keyWord=abc
 		MultivaluedMap<String, String> paras = uriInfo.getQueryParameters();
 		result = result + "Paras: categoryId=" + paras.getFirst("categoryId") + 
 				", keyWord=" + paras.getFirst("keyWord");
-		
+//		use this to use service search products by keyword
+//		http://docs.oracle.com/javaee/7/tutorial/jaxrs-client002.htm
 		return result;
 	}
 }

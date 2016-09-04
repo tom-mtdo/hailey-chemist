3d0cdcf05227a7c45eb4d05ff8b0578b5f5e9243
Tom Do <tom@sisuwellness.com>
2016-09-04 12:08:08 +1000

    on going

--- target/hailey-chemist/resources/js/app/views/desktop/product-search.js
+++ target/hailey-chemist/resources/js/app/views/desktop/product-search.js
@@ -7,20 +7,33 @@ define([
     'jquery',
     'bootstrap',
     'configuration',
-    'text!../../../../templates/desktop/product-search.html'
+    'text!../../../../templates/desktop/product-search.html',
+    'text!../../../../templates/desktop/product-count-by-category.html'
 ], function (utilities,
 		jquery,
 		bootstrap,
 		config,
-		productSearchTemplate) {
+		productSearchTemplate,
+		productCountByCategoryTemplate) {
 	
 	var ProductSearchView = Backbone.View.extend({
 		render:function(){
 			var self = this;
 			utilities.applyTemplate( $(self.el), productSearchTemplate, {} );
+			self.productCountByCategoryView = new ProductCountByCategoryView({model:{}, el:$("#divProductCountByCategory") });
+			self.productCountByCategoryView.render();
 			return self;
 		}
 	});
+
+	var ProductCountByCategoryView = Backbone.View.extend({
+		render:function(){
+			var self = this;
+			utilities.applyTemplate( $(self.el), productCountByCategoryTemplate, {} );
+			return self;
+		}
+	});
+
 	
 	return ProductSearchView;
 	

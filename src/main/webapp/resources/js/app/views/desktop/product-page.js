4f56e2afe1f06be9998f100ea911559c38ba94b5
Tom Do <tom@sisuwellness.com>
2016-10-09 09:10:48 +1100

    clear quantity after added to cart

--- src/main/webapp/resources/js/app/views/desktop/product-page.js
+++ src/main/webapp/resources/js/app/views/desktop/product-page.js
@@ -39,13 +39,14 @@ define([
     		// a line in cart
     		var quantity = $("#quantity"+ productId).val();   
     		
-    		alert("Quantity: " + quantity);
     		var cartLine = {"productId":productId, "productName":productName, "quantity":quantity};
     		utilities.addToCart(cartLine);    		
 
         	var event_bus = utilities.getEventBus();
         	event_bus.trigger('cartAdded');
-    		
+        	
+        	$("#quantity"+ productId).val('');
+        	
     		alert(productName + ' added to cart!');
     	}
 
